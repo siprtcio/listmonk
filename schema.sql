c0febc58cffa55c7f178a43762b8ef15754579b7
Yash Vaghasiya Siprtc <yash@o365.siprtc.io>
2025-07-15 17:56:08 +0530

    schema file change

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ DROP TABLE IF EXISTS subscribers CASCADE;
 CREATE TABLE subscribers (
     id              SERIAL PRIMARY KEY,
     uuid uuid       NOT NULL UNIQUE,
-    email           NULL,
+    email           TEXT,
     name            TEXT NOT NULL,
     attribs         JSONB NOT NULL DEFAULT '{}',
     status          subscriber_status NOT NULL DEFAULT 'enabled',
